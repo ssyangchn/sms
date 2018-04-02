816bdc89f31cccfdaf89a164eb313647f9f96c3e
ssyang <webmail.alibaba-inc.com>
2018-04-02 10:12:07 +0800

    init

--- src/main/java/com/renjietech/sms/controller/IndexController.java
+++ src/main/java/com/renjietech/sms/controller/IndexController.java
@@ -2,13 +2,10 @@ package com.renjietech.sms.controller;
 
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.renjietech.sms.service.BaseService;
-import com.renjietech.sms.service.SubService;
 import com.renjietech.sms.util.Encrypt;
 import com.renjietech.sms.util.RestUtil;
 import com.renjietech.sms.vo.MsgVo;
 import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.ResponseEntity;
@@ -19,6 +16,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.text.MessageFormat;
 import java.util.*;
 
 @PropertySource(value = {"classpath:sms.properties"}, encoding = "UTF-8")
@@ -32,16 +30,16 @@ public class IndexController {
     private String appId;
     @Value("${smsTpl}")
     private String smsTpl;
-    private List<Map<String, Object>> smstpl;
+    private List<Map<String, String>> smstpl;
     @RequestMapping("/")
     public String index(ModelMap modelMap) {
         if (smstpl == null) {
             smstpl = new ArrayList<>();
-            HashMap map = null;
+            HashMap<String,String> map;
             String[] split = smsTpl.split(",");
             for (String s : split) {
                 String[] op = s.split("-");
-                map = new HashMap();
+                map = new HashMap<>();
                 map.put("id", op[0]);
                 map.put("text", op[1]);
                 smstpl.add(map);
@@ -58,8 +56,7 @@ public class IndexController {
         Assert.notNull(templateId, "请选择模板！");
         Assert.hasText(phones, "请输入手机号码！");
         String[] phone = phones.split("\r\n");
-        Set<String> set = new HashSet();
-        set.addAll(Arrays.asList(phone));
+        Set<String> set = new HashSet<>(Arrays.asList(phone));
 
         SmsRequest req = new SmsRequest();
         List<Tel> tels = new ArrayList<>();
@@ -67,38 +64,30 @@ public class IndexController {
         req.setTpl_id(templateId);
         req.setParams(Arrays.asList(vars));
         Iterator<String> iterator = set.iterator();
-        String errorMsg = "";
+        StringBuilder errorMsg = new StringBuilder();
         int num = 0;
         while (iterator.hasNext()) {
             String mobile = iterator.next().trim();
-            tels.add(new Tel(mobile));
-            if (tels.size() % 200 == 0) {
+            String[] split = mobile.split(",");
+            req.getParams().set(0, MessageFormat.format("货号{0}号",split[0]));
+            tels.add(new Tel(split[1]));
                 try {
                     num += doSend(req);
                 } catch (Exception e) {
-                    errorMsg += e.getMessage();
+                    errorMsg.append(e.getMessage());
                 }
                 tels.clear();
-            }
         }
-        if (tels.size() > 0) {
-            try {
-                num += doSend(req);
-            } catch (Exception e) {
-                errorMsg += e.getMessage();
-            }
-        }
-
-        if (!errorMsg.isEmpty()) {
+        if (errorMsg.length() > 0) {
             if (num > 0) {
-                errorMsg = "本次发送成功：" + num + "条;错误信息：" + errorMsg;
+                errorMsg.insert(0, "本次发送成功：" + num + "条;错误信息：");
             }
-            return ResponseEntity.ok(new MsgVo(false, errorMsg));
+            return ResponseEntity.ok(new MsgVo(false, errorMsg.toString()));
         }
         return ResponseEntity.ok(new MsgVo(true, "发送成功，本次发送" + num + "条"));
     }
 
-    public int doSend(SmsRequest req) {
+    private int doSend(SmsRequest req) {
         long time = System.currentTimeMillis() / 1000;
         req.setTime(time);
         StringBuilder sb = new StringBuilder("appkey=");
@@ -147,7 +136,7 @@ public class IndexController {
         String nationcode = "86";
         String mobile;
 
-        public Tel(String mobile) {
+        Tel(String mobile) {
             this.mobile = mobile;
         }
     }
